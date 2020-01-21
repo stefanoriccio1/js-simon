1183cf8b6a8cbfa9df380fbf89f018e2f116ba71
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-21 15:28:14 +0100

    fatta funzione per la richiestadei numeri

--- js/script.js
+++ js/script.js
@@ -4,10 +4,20 @@ for (var i = 0; i < 5; i++) {
   listaNumeriRandom.push(generator(1, 100));
 }
 alert (listaNumeriRandom);
+
+setTimeout(richiestaNumeri, 1000);
+
+
 // funzioni
 function generator (min, max){
   min = Math.ceil(min);
   max = Math.floor (max);
   var random = Math.floor(Math.random() * (max - min + 1)) + min;
   return random;
+}
+
+function richiestaNumeri() {
+  for (var i = 0; i < listaNumeriRandom.length; i++) {
+  numeriUtente.push(parseInt(prompt('inserisci uno dei numeri visualizzati prima')));
+  }
 }
