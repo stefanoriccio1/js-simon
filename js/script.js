bd870d45b6991f303651e7f7f6d954e6d6017482
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-21 16:30:05 +0100

    tentativo check risposte

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 var listaNumeriRandom = [];
+var risposteUtente = richiestaNumeri;
+var risultato;
 
 for (var i = 0; i < 5; i++) {
   listaNumeriRandom.push(generatore(1, 100));
 }
-// alert (listaNumeriRandom);
-//
-// setTimeout(richiestaNumeri, 1000);
-//
-console.log(listaNumeriRandom);
-// console.log(setTimeout(richiestaNumeri, 1000));
+alert (listaNumeriRandom);
 
 // crea lista risposte utente
 
-console.log(setTimeout(richiestaNumeri, 1000));
+setTimeout(risposteUtente, 3000);
+
+if (risposteUtente == listaNumeriRandom){
+  risultato = alert('complimenti! hai indovinato tutti i numeri');
+}
+else{
+ risultato = alert ('mi dispiace, hai perso, ecco il tuo risultato:' + ' Numeri PC:' + listaNumeriRandom[i] + ' I tuoi Numeri:' + risposteUtente[i]);
+}
 
 
 // funzioni
@@ -27,8 +31,8 @@ function richiestaNumeri() {
   var risposte =[];
   var i= 0;
   while (i < listaNumeriRandom.length) {
-  risposte.push(parseInt(prompt('inserisci uno dei numeri visualizzati prima')));
-  i++;
-};
-return risposte;
+   risposte.push(parseInt(prompt('inserisci uno dei numeri visualizzati prima')));
+   i++;
+  }
+  alert(risposte);
 }
